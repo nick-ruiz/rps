1d8a86421de213a491398bd79493b7d772297b27
Nick <nickruiz91@gmail.com>
2019-03-10 02:21:31 -0700

    Fixed react environment variables

--- client/src/config/keys_prod.js
+++ client/src/config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
+  STRIPE_API_KEY: process.env.REACT_APP_STRIPE_API_KEY,
+  GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY
 };
